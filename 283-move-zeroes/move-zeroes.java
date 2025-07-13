45dff5375c542985ebcd95fca004e554daaa36a4
Shubh Vishwakarma <shubhvishwakarma0000000@gmail.com>
2025-07-13 23:32:13 +0530

    Time: 1 ms (99.75%) | Memory: 45.9 MB (86.67%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,17 +3,14 @@ class Solution {
         int n = nums.length;
         int[] temp = new int[n];
         int index = 0;
-
         for (int i = 0; i < n; i++) {
             if (nums[i] != 0) {
                 temp[index++] = nums[i];
             }
         }
-
         for (int i = 0; i < index; i++) {
             nums[i] = temp[i];
         }
-
         for (int i = index; i < n; i++) {
             nums[i] = 0;
         }
