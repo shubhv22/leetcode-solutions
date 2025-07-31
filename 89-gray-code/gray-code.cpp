0fe379805cd7c29e12972c24b90baa19bb0272e7
Shubh Vishwakarma <shubhvishwakarma0000000@gmail.com>
2025-08-01 01:41:55 +0530

    Time: 2 ms (34.38%) | Memory: 14.7 MB (54.72%) - LeetSync

---
+++ 89-gray-code/gray-code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        int size = 1 << n;
+        for (int i = 0; i < size; i++) {
+            result.push_back(i ^ (i >> 1));
+        }
+        return result;
+    }
+};
